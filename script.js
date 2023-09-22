bad6899cb75a03fb2aef6ec94adf5f19dd50849a
Pavel Kirilin <kirilin.pav@gmail.com>
2023-09-22 15:08:03 +0300

    Removed work projects

--- script.js
+++
@@ -1,7 +0,0 @@
-const viewProjectsButton = document.getElementById('view-projects-button');
-
-viewProjectsButton.addEventListener('click', () => {
-  const section = document.getElementById('personal-projects-section');
-  section.scrollIntoView({ behavior: 'smooth' });
-  viewProjectsButton.blur();
-});
